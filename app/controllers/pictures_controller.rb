645a6a3ee4a358cd4bd32e5c40caeb010852c6e5
matt brandt <mattbrandt87@gmail.com>
2014-05-19 14:17:43 -0500

    UPdate

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -1,4 +1,26 @@
 class PicturesController < ApplicationController
-  def show
+  def index
+    @list_of_pictures = Picture.all
   end
+
+  def show
+    # Your code goes here
+    # Use the information after the slash in the URL to look up the director with the corresponding ID number
+    # Store the director in the @director instance variable so the view can format it
+
+    @picture = Picture.find_by({ :id => params[:id] })
+  end
+
+  def create
+
+    @p = Picture.new
+    @p.id= params[:id]
+    @p.caption = params[:zebra]
+    @p.image_url = params[:image_url]
+    @d.save
+
+    redirect_to("http://localhost:3000/pictures")
+
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,20 @@
 Rxngif::Application.routes.draw do
   get("/picture_details/:id", { :controller => "pictures", :action => "show" })
+  get("/all_pictures", { :controller => "pictures", :action => "index" })
+
+  # CREATE
+  get("/pictures/new", { :controller => "pictures", :action => "new"        })
+  get("/create_picture", { :controller => "pictures", :action => "create"   })
+
+  # READ
+  get("/pictures",     { :controller => "pictures", :action => "index"      })
+  get("/pictures/:id", { :controller => "pictures", :action => "show"       })
+
+  # UPDATE
+  get("/pictures/:id/edit", {:controller => "pictures", :action => "edit" })
+  get("/update_picture/:id", {:controller => "pictures", :action => "update" })
+
+  # DELETE
+  get("/delete_picture/:id", {:controller => "pictures", :action => "destroy" })
+
 end
